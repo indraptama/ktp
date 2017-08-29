8c7a81310731d089ebc9dcaa93abd0da698f6da3
Indra Pratama Putra <indra.symfonia@gmail.com>
2017-08-29 20:30:57 +0700

    remove

--- README.md
+++
@@ -1,3 +0,0 @@
-# ktp
-
-Describe ktp here.
--- public/preact.config.js
+++
@@ -1,4 +0,0 @@
-export default function (config, env, helpers) {
-    const BASE_URL = env.production ? '/my-gh-pages-url' : ''
-    config.output.publicPath = BASE_URL
-}
