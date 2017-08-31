a346137131ad2a0f3dbf5c6e2354df54afda8f37
Indra Pratama Putra <indra.symfonia@gmail.com>
2017-08-31 08:19:58 +0700

    update

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {observable} from "mobx";
 import {observer} from 'mobx-react';
 
+
 @observer class Counter extends React.Component {
   @observable counter= 0;
 
@@ -19,6 +20,7 @@ import {observer} from 'mobx-react';
         <div>
           <p className="mb2 tc">Test MOBX Counter</p>
           <p className="mb4 tc f1 fw3">{this.counter}</p>
+
         </div>
         <div className="flex w-100">
           <button className="w-50" onClick={this.decrement.bind(this)}>-</button>
